f50274f1f596edbe0a1ff676146b0d535865c672
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-03-14 20:09:19 +0200

    Introduced new microservices initially by just replicating the same source code.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

---
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/app/base/AbstractLogComponent.java
@@ -0,0 +1,15 @@
+package gr.codelearn.spring.cloud.showcase.app.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+
+public abstract class AbstractLogComponent {
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+
+	@PostConstruct
+	private void init() {
+		logger.trace("Loaded {}.", getClass().getName());
+	}
+}
