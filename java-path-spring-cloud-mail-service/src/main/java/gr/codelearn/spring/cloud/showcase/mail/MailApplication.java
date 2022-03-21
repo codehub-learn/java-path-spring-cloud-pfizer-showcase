5c7d6a223e16261d63e235b279e0711af33dbf04
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-03-21 17:36:18 +0200

    Introducing service discovery using Netflix Eureka and register all services as clients.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-cloud-mail-service/src/main/java/gr/codelearn/spring/cloud/showcase/mail/MailApplication.java
+++ java-path-spring-cloud-mail-service/src/main/java/gr/codelearn/spring/cloud/showcase/mail/MailApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ComponentScan(basePackages = {"gr.codelearn.spring.cloud.showcase.core", "gr.codelearn.spring.cloud.showcase.mail"})
+@EnableDiscoveryClient
 public class MailApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MailApplication.class, args);
--- java-path-spring-cloud-order-service/src/main/java/gr/codelearn/spring/cloud/showcase/order/OrderApplication.java
+++ java-path-spring-cloud-order-service/src/main/java/gr/codelearn/spring/cloud/showcase/order/OrderApplication.java
@@ -2,12 +2,14 @@ package gr.codelearn.spring.cloud.showcase.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"gr.codelearn.spring.cloud.showcase.core", "gr.codelearn.spring.cloud.showcase.order"})
 @EnableJpaRepositories(namedQueriesLocation = "classpath:jpa-named-queries.properties")
+@EnableDiscoveryClient
 public class OrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
