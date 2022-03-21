78a310bff956450065ca6cfc1f5991e6f6cbab39
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-03-21 18:43:23 +0200

    Enabled scheduled cache eviction.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/CatalogApplication.java
+++ java-path-spring-cloud-catalog-service/src/main/java/gr/codelearn/spring/cloud/showcase/catalog/CatalogApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"gr.codelearn.spring.cloud.showcase.core", "gr.codelearn.spring.cloud.showcase.catalog"})
 @EnableDiscoveryClient
+@EnableScheduling
 public class CatalogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
