9a539369fd285c05605fd8b39307b0941a7cd5d5
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-03-21 20:36:02 +0200

    Test profile alignment with profile naming convention (generate-*).
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-cloud-mail-service/src/main/java/gr/codelearn/spring/cloud/showcase/mail/bootstrap/SampleMailRunner.java
+++ java-path-spring-cloud-mail-service/src/main/java/gr/codelearn/spring/cloud/showcase/mail/bootstrap/SampleMailRunner.java
@@ -7,7 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("sample-mail")
+@Profile("generate-mail")
 @Component
 @RequiredArgsConstructor
 public class SampleMailRunner extends AbstractLogComponent implements CommandLineRunner {
